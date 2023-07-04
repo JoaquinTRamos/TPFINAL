fc2a01afa828c50d50b570d4cd1374b15bdebb93
JoaquinTRamos <ramossjoaquinn@gmail.com>
2023-07-04 20:44:31 -0300

    Generacion de Mensajes para Paquete

--- Final EDP/Final EDP/Metadata.py
+++ Final EDP/Final EDP/Metadata.py
@@ -1,2 +1,10 @@
+# Contiene toda la informacion de un paquete
 class Metadata():
+
+    def __init__(self, timestamp, origen:int, destino: int) -> None:
+        self.timestamp = timestamp
+        self.origen = origen
+        self.destino = destino
+        pass
+    
     pass
--- Final EDP/Final EDP/Paquete.py
+++ Final EDP/Final EDP/Paquete.py
@@ -1,14 +1,22 @@
 # crear la clase paquete con los siguientes atributos: Timestamp, metadata (objeto de la clase metadata), y el mensaje
 from Metadata import Metadata
+from lorem_text import lorem
+from random import randint
 
 
 class Paquete():
-    def __init__(self, timestamp, metadata:Metadata, mensaje:str):
-        self.timestamp = timestamp # Para mi esto deberia formar parte del Metadata.
+    def __init__(self, id: int, metadata: (Metadata|None)):
+        self.id = id
         self.metadata:Metadata = metadata
-        self.mensaje:str = mensaje
+        self.mensaje:str = lorem.words(randint(1,25))
 
     def __str__(self):
-        return f"Paquete: {self.timestamp}, {self.metadata}, {self.mensaje}"
+        return f"Paquete {self.id}:, {self.metadata}, {self.mensaje}"
 
+
+# TESTING - TESTING - TESTING
+if __name__ == "__main__":
+    
+    for i in range(3):
+        print(Paquete(i,None))
     
